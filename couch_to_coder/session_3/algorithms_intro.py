00f4aae7a374ec26e8cfdf33563825eda8cfb457
Omosefe-Osakue <queenettemarie@gmail.com>
2023-08-29 21:17:20 +0100

    Couch to coder python exercises week 1-3

---
+++ couch_to_coder/session_3/algorithms_intro.py
@@ -0,0 +1,45 @@
+# A Simple programme to simulate
+# a Bank ATM machine
+
+#Initialize the variables
+balance = 10000
+pin = 2324
+tries = 3
+
+
+#Ask the user to input their pin
+userPin = input("Enter your Pin: ")
+
+while(tries > 0):
+    #Check if user input is a valid digit
+    if(not userPin.isdigit()):
+        print("What you have entered above is not a digit")
+        break
+
+    #Check if the user pin is correct
+    if pin == int(userPin):
+        userAction= input("Would you like to make a Withdrawal (W), Deposit (D) or see your account Balance (B) : ").lower()
+        
+        #Get user's preferred action and execute it
+        if userAction == "w":
+            amount = float(input("How much would you like to withdraw: ")) 
+            balance = balance - amount
+            print(f"Your updated account balance is £{balance}")
+            break
+        elif userAction == "d":
+            amount = float(input("How much would you like to Deposit: ")) 
+            balance = balance + amount
+            print(f"Your updated account balance is £{balance}")
+            break
+        else:
+            print(f"Your account balance is £{balance}")
+            break
+
+    else:
+        tries -= 1
+        #guard clause
+        if(tries == 0):
+            break
+        userPin = (input("You have entered the wrong pin. Please try again : "))
+
+    
